6e7e76d9aa9de743dd8cd3dd4f587dc7b9b6bc4e
Yunus Alp Özmen <yunus9095@gmail.com>
2024-03-03 14:25:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double tutar, kdvOran = 0.18, kdvTutar, kdvliTutar;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ücret Tutarını Girniz");
+        tutar = input.nextDouble();
+
+        kdvTutar = tutar * kdvOran;
+        kdvliTutar = tutar + kdvTutar;
+
+        System.out.println("KDV'siz Tutar :" + tutar);
+        System.out.println("KDV Oranı :" + kdvOran);
+        System.out.println("KDV Tutarı :" + kdvTutar);
+        System.out.println("KDV'li Tutarı :" + kdvliTutar);
+
+    }
+}
